eb2c6b08604ead4218037e5671473349a672640b
shawn <xw2002121@gmail.com>
2024-07-08 19:51:19 +0800

    feat: optimize the scheduler
    Signed-off-by: shawn <xw2002121@gmail.com>

--- scheduler.cc
+++ scheduler.cc
@@ -2,14 +2,26 @@
  * @file scheduler.cc
  * @brief 协程调度器实现
  * @version 0.1
- * @date 2021-06-15
+ * @date 2024-06-15
  */
 #include "scheduler.h"
 
-namespace coro {
+#include <cassert>
 
 static thread_local Scheduler* t_scheduler = nullptr;
 static thread_local Fiber* t_scheduler_fiber = nullptr;
+static thread_local int s_thread_id = 0;
+
+// 获取调度器指针
+Scheduler* Scheduler::GetThis() { return t_scheduler; }
+
+void Scheduler::SetThis() { t_scheduler = this; }
+
+Fiber* Scheduler::GetSchedulerFiber() { return t_scheduler_fiber; }
+
+int Scheduler::GetThreadId() { return s_thread_id; }
+
+void Scheduler::SetThreadId(int thread_id) { s_thread_id = thread_id; }
 
 Scheduler::Scheduler(size_t threads, bool use_caller, const std::string& name) {
     m_useCaller = use_caller;
@@ -22,4 +34,60 @@ Scheduler::Scheduler(size_t threads, bool use_caller, const std::string& name) {
     }
     m_threadCount = threads;
 }
-}  // namespace coro
+
+Scheduler::~Scheduler() {}
+
+void Scheduler::start() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (m_stopping) {
+        std::cerr << "Scheduler is stopped" << std::endl;
+        return;
+    }
+
+    assert(m_threads.empty());
+    m_threads.resize(m_threadCount);
+    for (size_t i = 0; i < m_threadCount; i++) {
+    }
+}
+
+void Scheduler::run() {
+    std::cout << "Scheduler::run() starts in thread: " << GetThreadId()
+              << std::endl;
+    SetThis();
+    if (GetThreadId() != m_rootThread) {
+        assert(t_scheduler_fiber == nullptr);
+        t_scheduler_fiber = Fiber::GetThis().get();
+    }
+}
+
+void Scheduler::tickle() { tickler++; }
+
+// 发布线程任务
+void Scheduler::scheduleLock(std::shared_ptr<Fiber> fc, int thread_id) {
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        SchedulerTask task;
+
+        task.fiber = fc;
+        task.cb = nullptr;
+        task.thread = thread_id;
+
+        m_tasks.push_back(task);
+    }
+    tickle();
+}
+
+// 发布函数任务
+void Scheduler::scheduleLock(std::function<void()> fc, int thread_id) {
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        SchedulerTask task;
+
+        task.fiber = nullptr;
+        task.cb = fc;
+        task.thread = thread_id;
+
+        m_tasks.push_back(task);
+    }
+    tickle();
+}
--- scheduler.h
+++ scheduler.h
@@ -8,17 +8,20 @@
 #ifndef __CORO_SCHEDULER_H__
 #define __CORO_SCHEDULER_H__
 
+#include <atomic>
 #include <list>
+#include <mutex>
+#include <string>
+#include <vector>
 
 #include "fiber.h"
 
-namespace coro {
+class Fiber;
+
+class Thread;
 
 class Scheduler {
    public:
-    typedef std::shared_ptr<Scheduler> ptr;
-    typedef Mutex MutexType;
-
     Scheduler(size_t threads = 1, bool use_caller = true,
               const std::string& name = "Scheduler");
 
@@ -26,22 +29,96 @@ class Scheduler {
 
     const std::string& getName() const { return m_name; }
 
+    static Scheduler* GetThis();
+    void SetThis();
+
+    static Fiber* GetSchedulerFiber();
+
+    void scheduleLock(std::shared_ptr<Fiber> fc, int thread_id = -1);
+    void scheduleLock(std::function<void()> fc, int thread_id = -1);
+
+    static int GetThreadId();
+    static void SetThreadId(int thread_id);
+
+    virtual void start();
+    virtual void stop();
+
+    virtual void tickle();
+
+   protected:
+    virtual void run();
+
+    virtual void idle();
+
+    virtual bool stopping() { return m_stopping; }
+
+    bool hasIdleThreads() { return m_idleThreadCount > 0; }
+
    private:
+    // 调度任务，协程/函数二选一，可指定在哪个线程上调度
+    struct SchedulerTask {
+        std::shared_ptr<Fiber> fiber;
+        std::function<void()> cb;
+        int thread;
+
+        SchedulerTask() {
+            fiber = nullptr;
+            cb = nullptr;
+            thread = -1;
+        }
+
+        SchedulerTask(std::shared_ptr<Fiber> f, int thr) {
+            fiber = f;
+            thread = thr;
+        }
+
+        SchedulerTask(std::shared_ptr<Fiber>* f, int thr) {
+            fiber.swap(*f);
+            thread = thr;
+        }
+
+        SchedulerTask(std::function<void()> f, int thr) {
+            cb = f;
+            thread = thr;
+        }
+
+        void reset() {
+            fiber = nullptr;
+            cb = nullptr;
+            thread = -1;
+        }
+    };
+
+   private:
+    // 协程调度器名称
     std::string m_name;
-    MutexType m_mutex;
-    std::vector<Thread::ptr> m_threads;
-    std::list<SchedulerTask> m_tasks;
+    // 互斥锁
+    std::mutex m_mutex;
+    // 线程池
+    std::vector<std::shared_ptr<Thread>> m_threads;
+    // 任务队列
+    std::vector<SchedulerTask> m_tasks;
+    // 线程池的线程ID数组
     std::vector<int> m_threadIds;
+    // 工作线程的数量，不包括use_caller主线程
     size_t m_threadCount = 0;
+    // 活跃的线程数
     std::atomic<size_t> m_activeThreadCount = {0};
+    // idle 线程数
     std::atomic<size_t> m_idleThreadCount = {0};
 
+    // 是否使用caller线程执行任务
     bool m_useCaller;
-    Fiber::ptr m_rootFiber;
+    // use_caller为true时，调度器所在线程的调度协程 -> 必须在类内持有
+    // 不然创建完就会被释放
+    std::shared_ptr<Fiber> m_rootFiber;
+    // 调度器所在线程的id
     int m_rootThread = 0;
+    // 用于唤醒在idle协程中的线程
+    std::atomic<int> tickler;
 
+   protected:
     bool m_stopping = false;
 };
-}  // namespace coro
 
 #endif
